38922e9e7dd48177642543299f22e8d6951e1141
aleung0806 <aleung0806@gmail.com>
2023-09-13 19:40:10 -0700

    google button redirect works with nginx

--- src/controllers/google.controller.ts
+++ src/controllers/google.controller.ts
@@ -6,16 +6,19 @@ export const login: Middleware = async (req, res, next) => {
 };
 
 export const callback: Middleware = async (req, res, next) => {
-  passport.authenticate('google', 
-   (err, user, _info) => {
-    if (err) { return next(err); }
-    if (!user) { 
-      return res.sendStatus(200)
-    }
-    req.login(user, () => {
-      res.status(200).send(user)
-    })
-  })(req, res, next)
+  passport.authenticate('google', { 
+    failureRedirect: 'http://localhost:3000/login',
+    successRedirect: 'http://localhost:3000/',
+  })
+  //  (err, user, _info) => {
+  //   if (err) { return next(err); }
+  //   if (!user) { 
+  //     return res.sendStatus(200)
+  //   }
+  //   req.login(user, () => {
+  //     res.status(200).send(user)
+  //   })
+  (req, res, next)
 };
 
 export const success: Middleware = async (req, res, _next) => {
